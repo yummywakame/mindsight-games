6c2eedd2f1dbfceb210f0b7bc0f5117a1ace0905
Olivia Meiring <5927823+yummywakame@users.noreply.github.com>
2024-10-14 23:43:03 -0600

    hamburger menu working now

--- src/HamburgerMenu.js
+++ src/HamburgerMenu.js
@@ -4,13 +4,33 @@ import { Link } from 'react-router-dom';
 import './css/HamburgerMenu.css';
 
 class HamburgerMenu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+    };
+  }
+
+  handleStateChange(state) {
+    this.setState({ menuOpen: state.isOpen });
+  }
+
+  closeMenu() {
+    this.setState({ menuOpen: false });
+  }
+
   render() {
     return (
-      <Menu>
-        <Link to="/">Home</Link>
-        <Link to="/instructions">Instructions</Link>
-        <Link to="/color-game">Color Game</Link>
-      </Menu>
+      <div className="hamburger-menu-container"> {/* New container */}
+        <Menu
+          isOpen={this.state.menuOpen}
+          onStateChange={(state) => this.handleStateChange(state)}
+        >
+          <Link to="/" onClick={() => this.closeMenu()}>Home</Link>
+          <Link to="/instructions" onClick={() => this.closeMenu()}>Instructions</Link>
+          <Link to="/color-game" onClick={() => this.closeMenu()}>Color Game</Link>
+        </Menu>
+      </div>
     );
   }
 }
