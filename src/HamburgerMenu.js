15812cff184740faf85eeaf8510158e909a76000
Olivia Meiring <5927823+yummywakame@users.noreply.github.com>
2024-10-15 01:00:26 -0600

    burger-squeeze menu added!

--- src/HamburgerMenu.js
+++ src/HamburgerMenu.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import { slide as Menu } from 'react-burger-menu';
+import React, { useState } from 'react';
+import Squeeze from '@animated-burgers/burger-squeeze';
+import '@animated-burgers/burger-squeeze/dist/styles.css'; // Ensure styles are imported
 import { Link } from 'react-router-dom';
 import './css/HamburgerMenu.css';
 
-class HamburgerMenu extends React.Component {
-  static defaultProps = {
-    customBurgerIcon: <img src="path/to/burger/icon.png" alt="Menu" />,
-    customCrossIcon: <img src="path/to/cross/icon.png" alt="Close" />
+function HamburgerMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+    console.log('Menu isOpen state:', !isOpen); // Debugging purpose
   };
 
-  render() {
-    return (
-      <Menu {...this.props}>
-        <Link to="/">Home</Link>
-        <Link to="/instructions">Instructions</Link>
-        <Link to="/preferences">Preferences</Link>
-        <Link to="/color-game">Color Game</Link>
-      </Menu>
-    );
-  }
+  return (
+    <div className="hamburger-container">
+      <Squeeze
+        isOpen={isOpen}
+        onClick={handleToggle}
+        color="#ffffff" // White color for visibility on black background
+        className="burger-icon"
+      />
+      {isOpen && (
+        <nav className="hamburger-menu">
+          <ul>
+            <li>
+              <Link to="/" onClick={handleToggle}>Home</Link>
+            </li>
+            <li>
+              <Link to="/instructions" onClick={handleToggle}>Instructions</Link>
+            </li>
+            <li>
+              <Link to="/preferences" onClick={handleToggle}>Preferences</Link>
+            </li>
+            <li>
+              <Link to="/color-game" onClick={handleToggle}>Color Game</Link>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </div>
+  );
 }
 
 export default HamburgerMenu;
-
